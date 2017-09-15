ba2cded6d885965be8f8532196dea5b503e65411
Fabrizio Sanmartino <fabrizio.sanmartino@gmail.com>
2017-09-15 11:41:15 +0200

    jsdom test removed

--- src/index.test.js
+++ src/index.test.js
@@ -8,14 +8,14 @@ describe('first test', () => {
    });
 });
 
-describe('index.html', () => {
-   it('should have Test JS environment', (done) => {
-      const index = fs.readFileSync('./src/index.html', 'utf-8');
-      jsdom.env(index, function(err, window) {
-         const h1 = window.document.getElementsByTagName('h1')[0];
-         expect(h1.innerHTML).to.equal("Test JS environment");
-         done();
-         window.close();
-      });
-   });
-})
+// describe('index.html', () => {
+//    it('should have Test JS environment', (done) => {
+//       const index = fs.readFileSync('./src/index.html', 'utf-8');
+//       jsdom.env(index, function(err, window) {
+//          const h1 = window.document.getElementsByTagName('h1')[0];
+//          expect(h1.innerHTML).to.equal("Test JS environment");
+//          done();
+//          window.close();
+//       });
+//    });
+// })
