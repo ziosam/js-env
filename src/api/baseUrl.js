a22ddb844b8b378de9354386e24cc556a718176f
Fabrizio Sanmartino <fabrizio.sanmartino@gmail.com>
2017-09-21 17:42:44 +0200

    Added surge for frontend deploy

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-   return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+   return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://shrouded-citadel-48456.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
